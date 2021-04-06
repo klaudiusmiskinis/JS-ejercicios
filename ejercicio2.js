5014e2b6664f28d721b766cbf4a00b31fdc873ef
klaudiusmiskinis <73123094+klaudiusmiskinis@users.noreply.github.com>
2021-04-06 12:43:58 +0200

    ejercicio2
    OnMouseOver(). Realiza una página que muestre cinco cuadrados de diferentes colores. Cuando el raton pase por encima de alguno de ellos el color de fondo será del color de relleno del cuadrado.
    Conversor monetario. Realiza una página que muestre un formulario para la conversión de Euros a Pesetas o viceversa. Los campos del formulario han de poder ser limpiados.
    Password. Realiza una página que muestre una entrada protegida con password a un web.

---
+++ ejercicio2.js
@@ -0,0 +1,76 @@
+//PARTE 1
+let body = document.getElementById('body');
+
+function cUno(){
+   body.style.backgroundColor = "tomato";
+}
+
+function cDos(){
+    body.style.backgroundColor = "thistle";
+ }
+
+ function cTres(){
+    body.style.backgroundColor = "violet";
+ }
+ 
+ function cCuatro(){
+    body.style.backgroundColor = "yellow";
+ }
+ 
+
+ function cCinco(){
+    body.style.backgroundColor = "yellowgreen";
+ }
+
+ function gris(){
+    body.style.backgroundColor = "grey";
+}
+
+
+//PARTE 2
+const pes = 0.006;
+
+function aEuros(){
+    let pesetas = document.getElementById('pesetas').value; 
+    pesetas = pesetas * pes;
+    document.getElementById('euros').value = pesetas.toFixed(2);
+}
+
+function aPesetas(){
+    let euros = document.getElementById('euros').value;
+    euros = euros / pes;
+    document.getElementById('pesetas').value = euros.toFixed(2);
+}
+ 
+function borrar() {
+    document.getElementById('euros').value = 0;
+    document.getElementById('pesetas').value = 0;
+}
+
+function cargarEur(){
+    aPesetas();
+}
+
+function cargarPes(){
+    aEuros();
+}
+
+//PARTE 3
+
+let user;
+let pass;
+
+const div = document.createElement('div');
+let capa = document.getElementsByClassName('verificar');
+capa.appendChild(div);
+
+function verificar() {
+    user = document.getElementById('user').value;
+    pass = document.getElementById('pass').value;
+    
+
+    //EL USUARIO DEBE SER 'ejdos' y la contraseña 'hecho'.
+    if (user == 'ejdos'){
+
+    }
+}
