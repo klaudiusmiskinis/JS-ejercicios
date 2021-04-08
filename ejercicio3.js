48dc9e3fda801359ff139fb7a2f0c6e4fe8c6fc1
klaudiusmiskinis <73123094+klaudiusmiskinis@users.noreply.github.com>
2021-04-08 16:53:00 +0200

    BORRANDO EJ3.JS

--- ejercicio3.js
+++
@@ -1,6 +0,0 @@
-document.onkeypress = function(event) {
-    event = event || window.event;
-    var letra = event.keyCode || event.which;
-    var cadena = String.fromCharCode(letra);
-    document.getElementById('output').innerHTML = cadena;
-}
