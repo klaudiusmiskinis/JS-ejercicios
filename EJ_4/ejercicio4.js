5e0c31191048ec474094731f16604869cab7efee
klaudiusmiskinis <73123094+klaudiusmiskinis@users.noreply.github.com>
2021-04-09 17:25:07 +0200

    Add files via upload

---
+++ EJ_4/ejercicio4.js
@@ -0,0 +1,13 @@
+
+let a = document.getElementById('cUno');
+let b = document.getElementById('cTres');
+
+function esconder( ){
+        a.style.display = "none";
+        b.style.display = "none";
+}
+
+function ver() {
+        a.style.display = "block";
+        b.style.display = "block";
+}
